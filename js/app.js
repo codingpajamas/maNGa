1c0ff1f13ff0f450d47a366e177de3a5580c4eba
kokikode <nanjiro.ko@gmail.com>
2015-08-13 16:55:48 +0800

    update controllers and pages

--- js/app.js
+++ js/app.js
@@ -11,31 +11,54 @@ angular.module('mangaApp', ['ngRoute', 'ngResource'])
 			})
 			.when('/popular', {
 				controller : 'PopularCtrl',
-				templateUrl : '/partials/grid.html'
+				templateUrl : '/partials/popular.html'
+			})
+			.when('/latest', {
+				controller : 'LatestCtrl',
+				templateUrl : '/partials/latest.html'
 			})
 			.when('/:anchor', {
 				controller : 'InfoCtrl',
 				templateUrl : '/partials/info.html'
 			})
 	}])
-	.factory('Manga', ['$resource', function($resource){
-		return $resource('https://www.mangaeden.com/api/list/0/?p=0&l=50') 
+	.factory('Manga', ['$resource', function($http){
+		return $http('https://www.mangaeden.com/api/list/0/?p=0&l=50') 
 	}])
 	.controller('MainCtrl', ['$scope', 'Manga', function($scope, Manga){
-		$scope.MangaList = [];
+		$scope.allManga = [];
+		$scope.popularManga = [];
+		$scope.latestManga = []; 
 
 		Manga.get({}, function(manga){
-			$scope.MangaList = manga.manga;
+			$scope.allManga = _.sortBy(manga.manga, 'a');
+			$scope.popularManga = _.sortByOrder(manga.manga, ['h'], ['desc']);
+			$scope.latestManga = _.sortByOrder(_.filter(manga.manga, 'ld'), ['ld'], ['desc']);  
+			$scope.$broadcast('websiteReady', true); 
 		}); 
 	}])
 	.controller('HomeCtrl', ['$scope', function($scope){
-		$scope.message = 'Welcome to Homepage';
+		$scope.homePopularManga =  _.slice($scope.popularManga, 0, 5);
+		$scope.homeLatestManga =  _.slice($scope.latestManga, 0, 5);
+
+		$scope.$on('websiteReady', function(event, data){ 
+			$scope.homePopularManga =  _.slice($scope.popularManga, 0, 5);
+			$scope.homeLatestManga =  _.slice($scope.latestManga, 0, 5);
+		})
 	}])
 	.controller('PopularCtrl', ['$scope', function($scope){
-		$scope.message = 'popular';
-
-			console.log($scope.MangaList);
+		$scope.mangaList = $scope.popularManga;   
+		$scope.$on('websiteReady', function(event, data){ 
+			$scope.mangaList = $scope.popularManga; 
+		})
+	}])
+	.controller('LatestCtrl', ['$scope', function($scope){
+		$scope.mangaList = $scope.popularManga;  
+		$scope.$on('websiteReady', function(event, data){ 
+			$scope.mangaList = $scope.popularManga;
+		})
 	}])
 	.controller('InfoCtrl', ['$scope', function($scope){
-		$scope.message = 'info'
+		$scope.message = 'info';
+		console.log($scope.latestManga);
 	}])
